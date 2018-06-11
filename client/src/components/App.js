a93c48ec259d2dd9933028ad3a166e07177c7b37
sleex89 <samlee723@gmail.com>
2018-06-10 20:20:51 -0700

    Implemented higher order component for auth

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,18 +3,19 @@ import { Route, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import * as actions from 'actions';
 
 class App extends Component {
   renderButton() {
     if(this.props.auth) {
       return (
-        <button>
+        <button onClick={() => this.props.changeAuth(false)}>
           Sign Out
         </button>
       )
     } else {
       return (
-        <button>
+        <button onClick={() => this.props.changeAuth(true)}>
           Sign In
         </button>
       )
@@ -53,4 +54,4 @@ function mapStateToProps(state) {
   return { auth: state.auth };
 }
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, actions)(App);
