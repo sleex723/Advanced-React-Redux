8d95949391d7005953f045bac1c86df4658bedd1
sleex89 <samlee723@gmail.com>
2018-06-10 18:48:58 -0700

    Setup of redux

--- client/src/components/__tests__/CommentBox.test.js
+++ client/src/components/__tests__/CommentBox.test.js
@@ -16,3 +16,22 @@ it('has a text area and a button', () => {
   expect(wrapped.find('textarea').length).toEqual(1)
   expect(wrapped.find('button').length).toEqual(1)
 })
+
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+    wrapped.update();
+  })
+
+  it('has a text area that users can type in it', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  })
+
+  it('it clears out the text area after clicking submit', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  })
+})
